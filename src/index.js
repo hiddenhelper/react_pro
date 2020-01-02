8626ebe06eda66a5a29651bc9676c82f9d5a5c1e
hiddenhelper <bountyhunter0308@hotmail.com>
2020-01-02 15:06:56 -0800

    master

--- src/index.js
+++ src/index.js
@@ -2,30 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
-    const labelText = 'Enter Name';
-    const btnText = { Text: 'Click Me'};
     return (
         <div className="ui container comments">
-            <CommentDetail 
-                author="sam" 
-                timeAgo="today 4" 
-                content="aa"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="alex" 
-                timeAgo="today 2" 
-                content="bb"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="Jane" 
-                timeAgo="yesterday 6" 
-                content="cc"
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <h3>hello</h3>
+                <div>welcome</div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="sam" 
+                    timeAgo="today 4" 
+                    content="aa"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail 
+                    author="alex" 
+                    timeAgo="today 2" 
+                    content="bb"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+           
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="yesterday 6" 
+                    content="cc"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>        
         </div>
     );
 };
