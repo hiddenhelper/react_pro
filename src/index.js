36f9ad3a8d18762dcc2b0bbe72c297b28ffbdf32
hiddenhelper <bountyhunter0308@hotmail.com>
2020-01-01 20:00:38 -0800

    first

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+
+const App = () => {
+    const labelText = 'Enter Name';
+    const btnText = { Text: 'Click Me'};
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={ faker.image.avatar() } />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sa
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
